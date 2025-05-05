a4fece4b30a4b4503ca0beaceb21332218771ed8
Dggrdg <kobe640670@gmail.com>
2025-05-05 10:30:16 +0800

    20250505

--- wtlcfilesystem/src/views/systemEdit.vue
+++ wtlcfilesystem/src/views/systemEdit.vue
@@ -8,12 +8,37 @@
                 機關設定
             </div>
             <div class="settingTable">
-                <el-table :data="tableData" stripe style="width: 100%">
+                <el-table :data="organData" stripe style="width: 100%">
                     <el-table-column type="index" width="50" />
                     <el-table-column prop="date" label="機關名稱" width="180" />
                     <el-table-column>
                         <template #header>
-                            <el-button type="success" @click="openDialog">新增</el-button>
+                            <el-button type="success" @click="openDialog('organ')">新增</el-button>
+                        </template>
+                        <template #default>
+                            <div style="display: flex;">
+                                <el-button type="info">編輯</el-button>
+                                <el-button type="danger">刪除</el-button>
+                            </div>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </div>
+            <div class="example-pagination-block">
+                <el-pagination layout="prev, pager, next" :total="1000" />
+            </div>
+        </div>
+        <div class="settingArea">
+            <div class="settingTitle">
+                合約類型設定
+            </div>
+            <div class="settingTable">
+                <el-table :data="contractTypeData" stripe style="width: 100%">
+                    <el-table-column type="index" width="50" />
+                    <el-table-column prop="date" label="機關名稱" width="180" />
+                    <el-table-column>
+                        <template #header>
+                            <el-button type="success" @click="openDialog('contractType')">新增</el-button>
                         </template>
                         <template #default>
                             <div style="display: flex;">
@@ -25,27 +50,20 @@
                 </el-table>
             </div>
         </div>
-        <div class="settingArea">
-            <div class="settingTitle">
-                合約類型設定
-            </div>
-            <div class="settingTable">
-                <el-table :data="contractTypeList" style="width: 100%">
-                    <el-table-column prop="date" label="Date" width="180" />
-                    <el-table-column prop="name" label="Name" width="180" />
-                    <el-table-column prop="address" label="Address" />
-                </el-table>
-            </div>
-        </div>
-        <el-dialog v-model="dialogVisible" title="新增機關" width="30%">
-            <el-form label-position="top" v-model="formData" ref="formRef" :rules="baseRules">
+        <el-dialog v-model="dialogVisible" :title="dialogTitle" width="30%">
+            <el-form label-position="top" v-show="organInputShow">
                 <el-form-item label="機關名稱">
-                    <el-input v-model="newOrganName" placeholder="請輸入機關名稱" />
+                    <el-input placeholder="請輸入機關名稱" v-model="organName" />
+                </el-form-item>
+            </el-form>
+            <el-form label-position="top" v-show="contractTypeInputShow">
+                <el-form-item label="合約類型">
+                    <el-input placeholder="請輸入合約類型" v-model="contractTypeName" />
                 </el-form-item>
             </el-form>
             <template #footer>
                 <el-button @click="closeDialog">取消</el-button>
-                <el-button type="primary" @click="saveNewOrgan">儲存</el-button>
+                <el-button @click="save" type="primary">儲存</el-button>
             </template>
         </el-dialog>
     </div>
@@ -55,28 +73,17 @@
 import { contractType, organ } from '@/model/model';
 import axios from 'axios';
 import { ElMessage } from 'element-plus';
-import { el } from 'element-plus/es/locale';
-import { ref, computed, onMounted } from 'vue';
+import { ref, computed, onMounted, vShow } from 'vue';
 
 const organList = ref<Array<organ>>([]);
-const contractTypeList = ref<Array<contractType>>([]);
-const formData = ref<{
-    organName: string,
-    contractType: string
-}>();
-const formRef = ref();
+const dialogTitle = ref("");
+const dialogVisible = ref(false);
+const organInputShow = ref(false);
+const contractTypeInputShow = ref(false);
+const organName = ref("");
+const contractTypeName = ref("");
 
-//表單驗證
-const baseRules = {
-    organName: [
-        { require, message: "機關名稱不得為空", trigger: "blur" }
-    ],
-    contractTypeName: [
-        { require, message: "機關名稱不得為空", trigger: "blur" }
-    ]
-}
-
-const tableData = [
+const organData = [
     {
         date: '2016-05-03',
         name: 'Tom',
@@ -106,35 +113,90 @@ const tableData = [
         date: '2016-05-01',
         name: 'Tom',
         address: 'No. 189, Grove St, Los Angeles',
-    },
+    }
 ]
 
-const dialogVisible = ref(false)  // 控制對話框顯示
+const contractTypeData = [
+    {
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    }
+]
 
-const newOrganName = ref('')      // 用來綁定輸入的機關名稱
+function openDialog(createType: string) {
+    if (createType == "organ") {
+        dialogTitle.value = "新增機關名稱";
+        contractTypeInputShow.value = false;
+        organInputShow.value = true;
+    } else if (createType == "contractType") {
+        dialogTitle.value = "新增合約類型"
+        contractTypeInputShow.value = true;
+        organInputShow.value = false;
+    }
+    dialogVisible.value = true;
+}
 
-function openDialog() {
-    dialogVisible.value = true
+function save() {
+    if (organInputShow.value) {
+        if (!organName.value) {
+            ElMessage.error('機關名稱不得為空');
+            return;
+        }
+        organData.push({
+            name: organName.value,
+            date: new Date().toISOString().split('T')[0], // 自動今天日期
+            address: '-'
+        });
+        ElMessage.success('新增機關成功');
+    }
+
+    if (contractTypeInputShow.value) {
+        if (!contractTypeName.value) {
+            ElMessage.error('合約類型不得為空');
+            return;
+        }
+        contractTypeData.push({
+            name: contractTypeName.value,
+            date: new Date().toISOString().split('T')[0],
+            address: '-'
+        });
+        ElMessage.success('新增合約類型成功');
+    }
+
+    // 清空
+    organName.value = '';
+    contractTypeName.value = '';
+    dialogVisible.value = false;
 }
 
 function closeDialog() {
-    dialogVisible.value = false
-}
-
-function saveNewOrgan() {
-    formRef.value.validate((valid: boolean) => {
-        if (valid) {
-            ElMessage({
-                type: 'success',
-                message: "儲存成功"
-            })
-        } else {
-            ElMessage({
-                type: 'warning',
-                message: "儲存失敗"
-            })
-        }
-    })
+    dialogVisible.value = false;
 }
 
 
